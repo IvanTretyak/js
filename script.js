34fa6c2b10b6a1510ca518a9ebf2191591c05d4d
Ivan Tretyak <ivan.t@g-code.cc>
2022-06-06 19:41:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let a="srting"
+const b=23
+let c=23.23
+
+let d=null
+let e=undefined
+let ee=[{lastname:"vasya",phone:"23424",address:{street:"2",city:"23"}},{lastname:"vasya2",phone:"23424",address:{street:"2",city:"23"}}]
+let user={lastname:"vasya",phone:"23424",address:{street:"2",city:"23"}} 
+console.log(ee[1].lastname)
+let array=[1,2,3,4,5,6,7,8,9]
+
+const func=(a,b)=> {return a+b}
+const res=func(a,b)
+console.log(res)
