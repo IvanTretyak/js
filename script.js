292fa5f2d26af8d05df078c5626ac6207d8da33c
Ivan Tretyak <ivan.t@g-code.cc>
2022-06-06 20:12:41 +0300

    erer

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let c=23.23
 
 let d=null
 let e=undefined
-let ee=[{lastname:"petya",phone:"23424",address:{street:"2"}},{lastname:"v",phone:"2",address:{street:"2",city:"23"}}]
+let ee=[{lastname:"pea",phone:"23424",address:{street:"2"}},{lastname:"v",phone:"2",address:{street:"2",city:"23"}}]
 let user={lastname:"vasya",phone:"23424",address:{street:"2",city:"23"}}
 console.log(ee[1].lastname)
 let array=[1,2,3,4,5,6]
